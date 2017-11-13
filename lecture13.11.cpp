6735c8a95a940ebbfb6ccaf3f0f23b9c6c82dc60
Evgeniya Okopnaya <foggyjandjane@gmail.com>
2017-11-13 21:27:58 +0300

    Containers: map, queues, deque, list...

--- lecture13.11.cpp
+++ lecture13.11.cpp
@@ -3,9 +3,8 @@
 #include <vector>
 #include <algorithm>
 #include <set>  // unordered map
-#include <deque>  // double-ended queue - это как два вектора растущих в разные стороны
-#include <list>  // двусвязный список - элементы хранятся разрозненно, а не подряд.
-                 // можно итерироваться но нельзя обращаться по индексу
+#include <deque>  // double-ended queue - like two vectors growing in opposite directions
+#include <list>  // doubly-linked list. You can't get an element by its index is not supported (but you can iterate)
 #include <stack>  // LIFO - last in - first out
 #include <queue>  // FIFO - first in - first out
 
@@ -22,7 +21,7 @@ int main () {
     if (it == freqs.end())
         std::cout << "'a' is not found\n";
     else
-        std::cout << (*it).first << " " << it->second << "\n";  // можно так, а можно - так. второе лучше
+        std::cout << (*it).first << " " << it->second << "\n";  // it is recommended to do it in "->" way
 
     std::vector<std::pair<char, int>> v(
         freqs.begin(), freqs.end()
@@ -56,7 +55,7 @@ int main () {
         std::cout << c;
     std::cout << "\n";
 
-auto it = symbols.lower_bound('a');  // найдет первый >= a
+auto it = symbols.lower_bound('a');  // will find first symbol >= a
     if (it == symbols.end())
         std::cout << "'a' is not found\n";
     else
@@ -67,11 +66,11 @@ auto it = symbols.lower_bound('a');  // найдет первый >= a
     d.push_back(11);
     d.push_front(-1);
 
-    for (int x : d)  // можно выводить все элементы так
+    for (int x : d)  // you can print all the elements this way
         std::cout << x << " ";
     std::cout << "\n";
 
-    for (size_t i = 0; i != d.size(); ++i)  // а можно выводить и обращаться к индексам так
+    for (size_t i = 0; i != d.size(); ++i)  // or this way
         std::cout << d[i] << " ";
     std::cout << "\n";
 
@@ -81,7 +80,7 @@ auto it = symbols.lower_bound('a');  // найдет первый >= a
     s.push(14);
     s.push(15);
 
-    std::cout << s.top() << "\n";  //  а в очереди это не top, а front
+    std::cout << s.top() << "\n";  //  "front" instead of "top" in queues
 
     s.pop();
     s.pop();
@@ -94,5 +93,5 @@ auto it = symbols.lower_bound('a');  // найдет первый >= a
     pq.push(12);
     pq.push(3);
 
-    std::cout << pq.top();  // извлекает максимальный элемент
+    std::cout << pq.top();  // extracts the biggest number
 }

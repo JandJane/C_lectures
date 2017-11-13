49bf00201bc2bd3230c9d336d33016aa2ec8d665
Evgeniya Okopnaya <foggyjandjane@gmail.com>
2017-11-13 18:58:53 +0300

    Containers - map, queues, deque, etc.

---
+++ lecture13.11.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <set>  // unordered map
+#include <deque>  // double-ended queue - это как два вектора растущих в разные стороны
+#include <list>  // двусвязный список - элементы хранятся разрозненно, а не подряд.
+                 // можно итерироваться но нельзя обращаться по индексу
+#include <stack>  // LIFO - last in - first out
+#include <queue>  // FIFO - first in - first out
+
+
+int main () {
+    std::unordered_map<char, int> freqs;
+    std::string s;
+    std::getline(std::cin, s);
+
+    for (char c : s)
+        ++freqs[c];
+
+    auto it = freqs.find('a');
+    if (it == freqs.end())
+        std::cout << "'a' is not found\n";
+    else
+        std::cout << (*it).first << " " << it->second << "\n";  // можно так, а можно - так. второе лучше
+
+    std::vector<std::pair<char, int>> v(
+        freqs.begin(), freqs.end()
+    );
+
+    std::sort(
+        v.begin(),
+        v.end(),
+        [](const auto& a, const auto&  b) {
+              return a.second > b.second ||
+              a.second == b.second &&
+              a.first > b.second;
+        }
+    );
+
+    for (const auto& item : v) {
+        std::cout << item.first << " " << item.second << "\n";
+    }
+    std::string s;
+    getline(std::cin, s);
+
+    std::set<char> symbols(s.begin(), s.end());
+
+    auto it = symbols.find('a');
+    if (it == symbols.end())
+        std::cout << "'a' is not found\n";
+    else
+        std::cout << "found!\n";
+
+    for (char c : symbols)
+        std::cout << c;
+    std::cout << "\n";
+
+auto it = symbols.lower_bound('a');  // найдет первый >= a
+    if (it == symbols.end())
+        std::cout << "'a' is not found\n";
+    else
+        std::cout << it->first;
+
+    std::deque<int> d = {1, 2, 3, 4};
+
+    d.push_back(11);
+    d.push_front(-1);
+
+    for (int x : d)  // можно выводить все элементы так
+        std::cout << x << " ";
+    std::cout << "\n";
+
+    for (size_t i = 0; i != d.size(); ++i)  // а можно выводить и обращаться к индексам так
+        std::cout << d[i] << " ";
+    std::cout << "\n";
+
+    std::stack<int> s;
+
+    s.push(3);
+    s.push(14);
+    s.push(15);
+
+    std::cout << s.top() << "\n";  //  а в очереди это не top, а front
+
+    s.pop();
+    s.pop();
+
+    std::cout << s.top() << "\n";
+
+    std::priority_queue<int> pq;
+
+    pq.push(15);
+    pq.push(12);
+    pq.push(3);
+
+    std::cout << pq.top();  // извлекает максимальный элемент
+}
